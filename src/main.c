0a74935deb346df4cad8cae508311dbee88f80f7
anshieeeka <anshikabisht1202@gmail.com>
2026-07-04 19:51:17 +0000

    Initialize basic shell REPL

--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    
+
   setbuf(stdout, NULL);
-  printf("$ ");
   
+  while(1){
+
+    printf("$ ");
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+
+    if (strcmp(command, "exit") == 0) {
+      break;
+    }
+    else if (strncmp(command, "echo ", 5) == 0) {
+    printf("%s\n", command + 5);
+    }
+    else {
+      printf("%s: command not found\n", command);
+    }
+  }
   return 0;
-}
+  }
+
